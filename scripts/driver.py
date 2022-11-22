6b4c4821f350f09dc5267ae167afcffa237bfca2
Michel Metran <michelmetran@gmail.com>
2022-11-22 15:40:33 -0300

    Ajustes

--- scripts/driver.py
+++ scripts/driver.py
@@ -5,22 +5,20 @@ out.22
 
 import os
 import time
+
 import requests
-
+from dotenv import dotenv_values, find_dotenv
+from paths import *
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.options import Options
-
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
-from paths import *
 # https://stackoverflow.com/questions/15607903/python-module-os-chmodfile-664-does-not-change-the-permission-to-rw-rw-r-bu
 
-# Credencials
-from dotenv import dotenv_values, find_dotenv
-#config = dotenv_values(find_dotenv(usecwd=False))
-#os.environ['GH_TOKEN'] = config['GH_TOKEN']
+# config = dotenv_values(find_dotenv(usecwd=False))
+# os.environ['GH_TOKEN'] = config['GH_TOKEN']
 
 
 # Set Options
@@ -30,10 +28,14 @@ options.set_preference('intl.accept_languages', 'pt-BR, pt')
 options.set_preference('browser.download.folderList', 2)
 options.set_preference('browser.download.manager.showWhenStarting', False)
 options.set_preference('browser.download.dir', str(input_path))
-options.set_preference('browser.helperApps.neverAsk.saveToDisk',
-                       'application/octet-stream, application/pdf, application/vnd.ms-excel')
-options.set_preference('general.useragent.override',
-                       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36')
+options.set_preference(
+    'browser.helperApps.neverAsk.saveToDisk',
+    'application/octet-stream, application/pdf, application/vnd.ms-excel',
+)
+options.set_preference(
+    'general.useragent.override',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+)
 options.set_preference('pdfjs.disabled', True)
 options.set_preference('plugin.scan.Acrobat', '99.0')
 options.set_preference('plugin.scan.plid.all', False)
@@ -44,7 +46,7 @@ options.set_preference('browser.download.forbid_open_with', True)
 # By default, all driver binaries are saved to user.home/.wdm folder. (0)
 # You can override this setting and save binaries to project.root/.wdm. (1)
 os.environ['WDM_LOCAL'] = '0'
-#print(os.getenv('WDM_LOCAL'))
+# print(os.getenv('WDM_LOCAL'))
 
 
 # Download Driver
@@ -59,8 +61,7 @@ print(f'>>>>>> {driver_binary}')
 # Create Driver
 driver = webdriver.Firefox(
     service=Service(
-        executable_path=driver_binary,
-        log_path=logs_path / 'geckodriver.log'
+        executable_path=driver_binary, log_path=logs_path / 'geckodriver.log'
     ),
     options=options,
 )
@@ -71,7 +72,8 @@ xpath_path = adds_path / 'xpath.xpi'
 xpath_path = xpath_path.absolute().resolve()
 if not xpath_path.is_file():
     r = requests.get(
-        'https://addons.mozilla.org/firefox/downloads/file/3588871/xpath_finder-1.0.2-fx.xpi')
+        'https://addons.mozilla.org/firefox/downloads/file/3588871/xpath_finder-1.0.2-fx.xpi'
+    )
 
     with open(xpath_path, 'wb') as f:
         f.write(r.content)
@@ -86,7 +88,7 @@ if __name__ == '__main__':
 
     # Close Connection
     time.sleep(4)
-    #driver.quit()
+    # driver.quit()
 
     # Message
-    #print('Driver close!!')
+    # print('Driver close!!')
